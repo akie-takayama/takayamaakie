14fb7a31860dfe53a8ca9abb2d922fc31c798d1b
akie takayama <kimchonghe1127@gmail.com>
2023-04-09 22:47:05 +0900

    課題1.30 自己紹介プログラムを作成①提出

---
+++ FirstJava/src/lesson30/main/Person.java
@@ -0,0 +1,30 @@
+package lesson30.main;
+
+class Person{
+	// インスタンスフィールドを定義
+	public static int count =0;
+	public String name;
+	public int age;
+	public double height;
+	public double weight;
+	
+
+	// コンストラクタを定義しインスタンスフィールドに値をセット
+	public Person(String name,int age, double height, double weight){
+		Person.count++;
+		this.name =name;
+		this.age =age;
+		this.height =height;
+		this.weight =weight;
+		}
+	
+	public double bmi() {
+		return this.weight/this.height/this.height;
+	}
+	
+	public void print() {
+		System.out.println("名前は"+this.name+"です");
+		System.out.println("年は"+this.age+"です");
+		System.out.println("BMIは"+Math.round(this.bmi())+"です");
+	}
+}
